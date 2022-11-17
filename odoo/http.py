f93a43ae13ee14a5163f50db09ea6c852dc7a8e1
akkygit <aakash.vishwakarma@oodles.io>
2022-11-17 12:13:30 +0530

    changes

--- odoo/http.py
+++ odoo/http.py
@@ -652,7 +652,7 @@ class JsonRequest(WebRequest):
                     _logger.exception("Exception during JSON request handling.")
             error = {
                 'code': 200,
-                'message': "Odoo Server Error",
+                'message': "CIDS Server Error",
                 'data': serialize_exception(exception),
             }
             if isinstance(exception, werkzeug.exceptions.NotFound):
