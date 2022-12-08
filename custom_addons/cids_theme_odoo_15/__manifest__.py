4ba5810fd42f4a0746b3f0bbd14816bd200f4bfd
akkygit <aakash.vishwakarma@oodles.io>
2022-12-08 15:13:57 +0530

    change

---
+++ custom_addons/cids_theme_odoo_15/__manifest__.py
@@ -0,0 +1,38 @@
+{
+    'name': 'CIDS DESIGN Theme',
+    'description': 'CIDS HOME PAGE',
+    'category': 'Theme',
+    'sequence': 10,
+    'version': '1.0',
+    'depends': ['website','web'],
+    'data': [
+        'views/assets.xml',
+        'views/header.xml',
+        'views/footer.xml',
+        'views/snippets/banner.xml',
+        'views/snippets/about_us.xml',
+        'views/snippets/about_us2.xml',
+        'views/snippets/accreditation.xml',
+        'views/snippets/case_studies.xml',
+        'views/snippets/designer_work.xml',
+        'views/snippets/services.xml',
+        'views/snippets/testimonal.xml'
+    ],
+    'images': [
+    ],
+    'assets':{
+        'web.assets_frontend': [
+            'cids_theme_odoo_15/static/src/scss/style.scss',
+            'cids_theme_odoo_15/static/src/scss/owl.scss',
+            'cids_theme_odoo_15/static/src/scss/owltheme.scss',
+            'cids_theme_odoo_15/static/src/js/cids.js',
+            'cids_theme_odoo_15/static/src/js/owl.js',
+
+        ],
+    },
+    'snippet_lists': {
+    },
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ custom_addons/cids_theme_odoo_15/static/src/js/cids.js
@@ -0,0 +1,188 @@
+odoo.define("cids_theme_odoo_15.cidsdes", function (require) {
+    var ajax = require("web.ajax");
+    var core = require('web.core');
+   $(document).ready(function() {
+   console.log("aman");
+                var owl = $('.owl-carousel.designwork');
+                owl.owlCarousel({
+                  margin: 10,
+                  nav: true,
+                  autoplay:true,
+                  loop: true,
+                  responsive: {
+                    0: {
+                      items: 2
+                    },
+                    600: {
+                      items: 3
+                    },
+                    1000: {
+                      items: 4
+                    }
+                  }
+                })
+              });
+
+               $(document).ready(function() {
+                var owl1 = $('.owl-carousel.testimonial');
+                owl1.owlCarousel({
+                  margin: 10,
+                  nav: true,
+                  autoplay:true,
+                  loop: true,
+                  responsive: {
+                    0: {
+                      items: 1
+                    },
+                    600: {
+                      items: 1
+                    },
+                    1000: {
+                      items: 1
+                    }
+                  }
+                });
+                 $( ".owl-prev").html('<img src="https://dev.cids.design/web/image/1433-e2b28c47/Element-arrow-2.svg"/>');
+   $( ".owl-next").html('<img src="https://dev.cids.design/web/image/1433-e2b28c47/Element-arrow-2.svg"/>');
+              });
+
+               $(document).ready(function() {
+                 var owl2 = $('.owl-carousel.services');
+                owl2.owlCarousel({
+                 center:true,
+                 margin: 20,
+                 nav: true,
+
+
+
+  items:2,
+  loop:true,
+  margin:10,
+  animateOut: 'slideInLeft',
+  animateIn: 'slideOutRight',
+  stagePadding: 50,
+                  responsive: {
+                    0: {
+                      items: 1
+                    },
+                    600: {
+                      items: 2
+                    },
+                    1000: {
+                      items: 2
+                    }
+                  }
+                  })
+              });
+
+
+
+               $(document).ready(function() {
+                var owl3 = $('.owl-carousel.luxuryCarsiuel');
+                owl3.owlCarousel({
+                  margin: 10,
+                  nav: true,
+                  autoplay:true,
+                  loop: true,
+                  responsive: {
+                    0: {
+                      items: 1
+                    },
+                    600: {
+                      items: 1
+                    },
+                    1000: {
+                      items: 1
+                    }
+                  }
+                });
+                 $( ".owl-prev").html('<img src="https://dev.cids.design/web/image/1433-e2b28c47/Element-arrow-2.svg"/>');
+   $( ".owl-next").html('<img src="https://dev.cids.design/web/image/1433-e2b28c47/Element-arrow-2.svg"/>');
+              });
+
+              $(document).ready(function() {
+                var owl4 = $('.owl-carousel.address');
+                owl4.owlCarousel({
+                  margin: 10,
+                  nav: true,
+                  autoplay:false,
+                  loop: true,
+
+                  responsive: {
+                    0: {
+                      items: 1
+                    },
+                    769: {
+                      items: 1
+                    },
+                    1000: {
+                      items: 1
+                    }
+                  }
+                });
+                 $( ".owl-prev").html('<img src="https://dev.cids.design/web/image/1433-e2b28c47/Element-arrow-2.svg"/>');
+   $( ".owl-next").html('<img src="https://dev.cids.design/web/image/1433-e2b28c47/Element-arrow-2.svg"/>');
+              });
+
+
+
+               $(document).ready(function() {
+                var owl5 = $('.owl-carousel.bannerwork');
+                owl5.owlCarousel({
+                  margin: 10,
+                  nav: true,
+                  autoplay:true,
+                  loop: true,
+                  responsive: {
+                    0: {
+                      items: 1
+                    },
+                    600: {
+                      items: 1
+                    },
+                    1000: {
+                      items: 1
+                    }
+                  }
+                })
+              });
+
+               $(document).ready(function() {
+
+               $('.humburger').click(function(){
+
+               	$('.header-links a').each(function(index, element) {
+			    	    $(element).delay(index*100).hide().fadeToggle(2000);
+			    	    });
+
+			    	  $('.header-links').toggleClass('swichmenu');
+
+               });
+               });
+
+               $(document).ready(function() {
+                var owl5 = $('.owl-carousel.bannerwork');
+                owl5.owlCarousel({
+                  margin: 10,
+                  nav: true,
+                  autoplay:true,
+                  loop: true,
+                  responsive: {
+                    0: {
+                      items: 1
+                    },
+                    600: {
+                      items: 1
+                    },
+                    1000: {
+                      items: 1
+                    }
+                  }
+                });
+                 $( ".owl-prev").html('<img src="https://dev.cids.design/web/image/1433-e2b28c47/Element-arrow-2.svg"/>');
+   $( ".owl-next").html('<img src="https://dev.cids.design/web/image/1433-e2b28c47/Element-arrow-2.svg"/>');
+              });
+
+
+
+})
