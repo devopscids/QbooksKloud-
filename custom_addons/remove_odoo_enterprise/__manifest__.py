3a8783d2b0d1268f3bcce39052ed940966b08424
akkygit <aakash.vishwakarma@oodles.io>
2022-12-14 14:05:44 +0530

    changes

--- custom_addons/remove_odoo_enterprise/__manifest__.py
+++ custom_addons/remove_odoo_enterprise/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2018 Eska Yazılım ve Danışmanlık A.Ş (www.eskayazilim.com.tr)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Remove Odoo Enterprise",
+    "name": "Remove CIDS Enterprise",
     "summary": "Remove enterprise modules and setting items",
     "version": "15.0.1.0.1",
     "category": "Maintenance",
-    "author": "Eska, Onestein, Odoo Community Association (OCA)",
+    "author": "Eska, Onestein, CIDS Community Association (OCA)",
     "website": "https://github.com/OCA/server-brand",
     "license": "AGPL-3",
     "depends": ["base"],
