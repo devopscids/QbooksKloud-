8cea375dc371a2f3ae6b0637d92fd9582dc700a4
Aman Sharma <aman@aman.sharma>
2023-02-17 17:24:02 +0530

    changes

--- custom_addons/cids_theme_odoo_15/controllers/main.py
+++ custom_addons/cids_theme_odoo_15/controllers/main.py
@@ -4,35 +4,35 @@ from odoo import http
 
 class DemoClass(http.Controller):
 
-    @http.route(['/'], type='http', auth="user", website=True)
+    @http.route(['/'], type='http', auth="public", website=True)
     def home_page(self, **post):
         return request.render("cids_theme_odoo_15.home")
 
-    @http.route(['/landing'], type='http', auth="user", website=True)
+    @http.route(['/landing'], type='http', auth="public", website=True)
     def landing_page(self, **post):
         return request.render("cids_theme_odoo_15.landing_page")
 
-    @http.route(['/commercial'], type='http', auth="user", website=True)
+    @http.route(['/commercial'], type='http', auth="public", website=True)
     def commercial_page(self, **post):
         return request.render("cids_theme_odoo_15.commercial")
 
-    @http.route(['/hospitality'], type='http', auth="user", website=True)
+    @http.route(['/hospitality'], type='http', auth="public", website=True)
     def hospitality_page(self, **post):
         return request.render("cids_theme_odoo_15.hospitality")
 
-    @http.route(['/ffe'], type='http', auth="user", website=True)
+    @http.route(['/ffe'], type='http', auth="public", website=True)
     def ffe_page(self, **post):
         return request.render("cids_theme_odoo_15.ffe")
 
-    @http.route(['/how_we_design'], type='http', auth="user", website=True)
+    @http.route(['/how_we_design'], type='http', auth="public", website=True)
     def how_we_design_page(self, **post):
         return request.render("cids_theme_odoo_15.how_we_design")
 
-    @http.route(['/about'], type='http', auth="user", website=True)
+    @http.route(['/about'], type='http', auth="public", website=True)
     def about_page(self, **post):
         return request.render("cids_theme_odoo_15.about")
 
-    @http.route(['/our_principles'], type='http', auth="user", website=True)
+    @http.route(['/our_principles'], type='http', auth="public", website=True)
     def our_principles_page(self, **post):
         return request.render("cids_theme_odoo_15.our_principles")
 
